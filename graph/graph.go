cb6c5fddc112da1e3c86fac75042907b4cb3db37
david <david@cloudcube.net>
2013-08-26 06:08:35 +0800

    modified graph/graph.go

--- graph/graph.go
+++ graph/graph.go
@@ -1,8 +1,21 @@
 package graph
 
 import (
-	"database/graph/driver"
 	"errors"
+	"github.com/cloudcube/database/graph/driver"
 )
 
-//var drivers = make(map[string])
+var drivers = make(map[string]driver.Driver)
+
+// Register makes a database driver available by provided name.
+// If Register is called twice with the same name or if driver is nil,
+// it panics.
+func Register(name string, driver driver.Driver) {
+	if driver == nil {
+		panic("graph:Register driver is nil")
+	}
+	if _, udp := drivers[name]; dup {
+		panic("graph:Register called twice for driver " + name)
+	}
+	drivers[name] = driver
+}
